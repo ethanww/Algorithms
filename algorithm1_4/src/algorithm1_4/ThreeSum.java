44271176d53032c891e40ea3a7fd320f5572f99f
ethanw <ethan_w@aliyun.com>
2016-09-27 09:04:49 +0800

    add my 1_4 exercise

--- algorithm1_4/src/algorithm1_4/ThreeSum.java
+++ algorithm1_4/src/algorithm1_4/ThreeSum.java
@@ -1,41 +1,66 @@
 package algorithm1_4;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.Stopwatch;
 
 public class ThreeSum {
 
-    // print distinct triples (i, j, k) such that a[i] + a[j] + a[k] = 0
-    public static void printAll(int[] a) {
-        int N = a.length;
-        for (int i = 0; i < N; i++) {
-            for (int j = i+1; j < N; j++) {
-                for (int k = j+1; k < N; k++) {
-                    if (a[i] + a[j] + a[k] == 0) {
-                        StdOut.println(a[i] + " " + a[j] + " " + a[k]);
-                    }
-                }
-            }
-        }
-    } 
+	// print distinct triples (i, j, k) such that a[i] + a[j] + a[k] = 0
+	public static void printAll(int[] a) {
+		int N = a.length;
+		for (int i = 0; i < N; i++) {
+			for (int j = i + 1; j < N; j++) {
+				for (int k = j + 1; k < N; k++) {
+					if (a[i] + a[j] + a[k] == 0) {
+						StdOut.println(a[i] + " " + a[j] + " " + a[k]);
+					}
+				}
+			}
+		}
+	}
 
-    // return number of distinct triples (i, j, k) such that a[i] + a[j] + a[k] = 0
-    public static int count(int[] a) {
-        int N = a.length;
-        int cnt = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = i+1; j < N; j++) {
-                for (int k = j+1; k < N; k++) {
-                    if (a[i] + a[j] + a[k] == 0) {
-                        cnt++;
-                    }
-                }
-            }
-        }
-        return cnt;
-    } 
+	// return number of distinct triples (i, j, k) such that a[i] + a[j] + a[k]
+	// = 0
+	public static int count(int[] a) {
+		int N = a.length;
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = i + 1; j < N; j++) {
+				for (int k = j + 1; k < N; k++) {
+					if (a[i] + a[j] + a[k] == 0) {
+						cnt++;
+					}
+				}
+			}
+		}
+		return cnt;
+	}
 
+	/*public static void main(String[] args) throws FileNotFoundException {
+		Scanner cin = new Scanner(new File(args[0]));
+
+		ArrayList<Integer> arrayList = new ArrayList<>();
+		while (cin.hasNext()) {
+			arrayList.add(cin.nextInt());
+		}
+		int[] a = new int[arrayList.size()];
+		
+		
+		for (int i = 0; i < arrayList.size(); i++) {
+			a[i] = arrayList.get(i);
+		}
+		cin.close();
+		Stopwatch timer = new Stopwatch();
+		int cnt = count(a);
+		StdOut.println("elapsed time = " + timer.elapsedTime());
+		StdOut.println(cnt);
+	}*/
     public static void main(String[] args)  { 
         int[] a = In.readInts(args[0]);
 
@@ -44,4 +69,4 @@ public class ThreeSum {
         StdOut.println("elapsed time = " + timer.elapsedTime());
         StdOut.println(cnt);
     } 
-} 
+}
--- algorithm1_4/src/algorithm1_4/console.java
+++ algorithm1_4/src/algorithm1_4/console.java
@@ -1,7 +1,18 @@
 package algorithm1_4;
 
+import java.io.FileNotFoundException;
+
 public class console {
 	public static void main(String[] args) {
-		Stopwatch.main(new String[]{"2000"});
+		//Stopwatch.main(new String[]{"2000"});
+		//ThreeSum.main(new String[]{1Kints.txt});
+//		try {
+//			ThreeSum.main(new String[]{"src/algorithm1_4/1Kints.txt"});
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+		ThreeSum.main(new String[]{"1Kints.txt"});
 	}
 }
